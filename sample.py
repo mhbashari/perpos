b51f3251619e69cd7c444dd03a2c4f5e34346008
hassan <mhbashari@gmail.com>
2017-12-23 23:39:46 +0330

    readme

--- sample.py
+++ sample.py
@@ -1,4 +1,5 @@
 from POS.POSTagger import POSTagger
 
-pos_tagger = POSTagger("/home/hassan/PycharmProjects/perpos_git/model/perpos.model")
-print(pos_tagger.parse("به گزارش گروه بین الملل خبرگزاری فارس، «بشار اسد» رئیس جمهور سوریه در جمع روزنامه نگاران روس گفت که هر گونه عملیات نظامی در سوریه بدون موافقت مقامات این کشور، تجاوز است؛ چه در رقه باشد یا در سایر مناطق.".split()))
+pos_tagger = POSTagger("model/perpos.model")
+tokens = "حلقه محاصره تروریست‌ها در جنوب سوریه تنگتر شد".split()
+pos_tagger.parse(tokens)
